5f8ed41bf001c90e3a0a35220c444564b3e87436
James Hooper <jameshooperdesigns@gmail.com>
2023-08-02 19:27:30 -0600

    Add more functionality to moving the character

--- app.js
+++ app.js
@@ -4,20 +4,25 @@ const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 
 let currentIndex = 76;
+const width = 9;
 
 function moveCharacter(e) {
   switch(e.key) {
     case 'ArrowLeft' : 
       console.log('move left');
+      currentIndex =- 1
       break
     case 'ArrowRight' : 
       console.log('move right');
+      currentIndex += 1
       break
     case 'ArrowUp' : 
       console.log('move up');
+      currentIndex -= width
       break  
     case 'ArrowDown' : 
       console.log('move down');
+      currentIndex += width
       break       
   }
   squares[currentIndex].classList.add('character')
