aeb64265828a61851b9da52283c89914a6496fce
James Hooper <jameshooperdesigns@gmail.com>
2023-08-02 19:18:02 -0600

    Add switch statement for moving keys

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const resultDisplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 
-function moveCharacter() {
-  console.log('moved')
+let currentIndex = 76;
+
+function moveCharacter(e) {
+  switch(e.key) {
+    case 'ArrowLeft' : 
+      console.log('move left');
+      break
+    case 'ArrowRight' : 
+      console.log('move right');
+      break
+    case 'ArrowUp' : 
+      console.log('move up');
+      break  
+    case 'ArrowDown' : 
+      console.log('move down');
+      break       
+  }
+  squares[currentIndex].classList.add('character')
 }
 
 document.addEventListener('keyup', moveCharacter);
